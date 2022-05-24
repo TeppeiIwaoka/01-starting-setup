56b6b88827875e5ff332df09a91c5b561590af21
TeppeiIwaoka <tep731kaizi@gmail.com>
2022-05-24 23:56:15 +0900

    refactor items list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,19 +16,6 @@ const Expenses = (props) => {
     return item.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -36,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
